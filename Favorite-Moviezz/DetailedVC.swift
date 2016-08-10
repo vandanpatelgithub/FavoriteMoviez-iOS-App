cf61918f255fc8f9aee6bbfa3f7592ad76fde1f4
Vandan Patel <vandan.ets@gmail.com>
2016-08-09 23:41:20 -0700

    Goes to DetailedVC when clicked on Cell.

---
+++ Favorite-Moviezz/DetailedVC.swift
@@ -0,0 +1,16 @@
+//
+//  DetailedVC.swift
+//  Favorite-Moviezz
+//
+//  Created by Preeti Patel on 09/08/16.
+//  Copyright © 2016 Vandan Patel. All rights reserved.
+//
+
+import UIKit
+
+class DetailedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
